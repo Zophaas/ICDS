8b51bb0dacc92ecba603dafad78fbb9df8d08feb
HughZhao <zhaohy24@mails.tsinghua.edu.cn>
2025-02-10 19:12:49 +0800

    upload vocabulary

--- tokenized/BPETrain.py
+++ tokenized/BPETrain.py
@@ -1,9 +1,6 @@
-import os
-from tokenizers import ByteLevelBPETokenizer
 from config import *
 import os
 from tokenizers import ByteLevelBPETokenizer
-import multiprocessing
 
 
 def train_tokenizer(input_file, vocab_size=5000):
@@ -61,4 +58,4 @@ def large_file_bpe_tokenization(input_file, output_dir, vocab_dir, chunk_size=10
     print(f"Large file tokenization complete. Output in {output_file}")
 '''
 
-large_file_bpe_tokenization(os.path.join(destination_dir,'merged.txt'), tokenized_dir, sample_dir)
+large_file_bpe_tokenization(os.path.join(destination_dir,'merged.txt'), tokenized_dir, tokenized_dir)
