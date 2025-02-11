021af3f002c2fd8dddebdf7a858c076c5176e451
Ziyan Deng <3220100107@zju.edu.cn>
2025-02-11 10:21:50 +0000

    modify skip-gram

--- representation/word2vec_sg.py
+++ representation/word2vec_sg.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 from os import path
 
 from gensim.models import Word2Vec
@@ -9,21 +7,35 @@ from config import *
 write_to_file = False
 
 tokens,token_ids = BPE_tokenize_file(destination_dir, tokenized_dir, write_to_file)
+
+sentences = []
+sentence = []
+for word in tokens:
+    if word == '.':
+        if sentence:
+            sentences.append(sentence)
+            sentence = []
+    else:
+        sentence.append(word)
+if sentence:
+    sentences.append(sentence)
+
+print("sentences are ready.")
+
 model = Word2Vec(vector_size=256, window=5, min_count=1, workers=4, sg=1)
 
-model.build_vocab(tokens)
+model.build_vocab(sentences)
 
 # 分步训练模型
 for epoch in range(10):  # 假设训练 10 轮
-    model.train(tokens, total_examples=model.corpus_count, epochs=1)
+    model.train(sentences, total_examples=model.corpus_count, epochs=1)
     print(f"Epoch {epoch + 1} completed")
 
 # 保存模型
-model.save('word2vec_model.bin')
+model.save("word2vec_sg.model")
 
 # 获得词向量
 word_vectors = model.wv
 
-#输出前 10 个词向量
 for word in word_vectors.index_to_key[:10]:
     print(f"Word: {word}, Vector: {word_vectors[word]}")
